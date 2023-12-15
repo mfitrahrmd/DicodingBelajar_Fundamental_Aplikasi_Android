4a18295a984beed4aaab45af68d08da265b33d4e
mfitrahrmd <tgcfitrah26@gmail.com>
2023-12-15 23:25:52 +0700

    navigation data with safeargs

--- app/src/main/java/com/mfitrahrmd/dicoding_belajar_fundamental_aplikasi_android/CategoryFragment.kt
+++ app/src/main/java/com/mfitrahrmd/dicoding_belajar_fundamental_aplikasi_android/CategoryFragment.kt
@@ -1,25 +1,18 @@
 package com.mfitrahrmd.dicoding_belajar_fundamental_aplikasi_android
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.mfitrahrmd.dicoding_belajar_fundamental_aplikasi_android.databinding.FragmentCategoryBinding
 
 class CategoryFragment : Fragment() {
     private lateinit var _binding: FragmentCategoryBinding
 
-    companion object {
-        val EXTRA_NAME = "extra_name"
-        val EXTRA_STOCK = "extra_stock"
-    }
-
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentCategoryBinding.inflate(inflater, container, false)
 
@@ -30,11 +23,11 @@ class CategoryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         _binding.btnToCategoryLifestyle.setOnClickListener {
-            with(Bundle()) {
-                this.putString(EXTRA_NAME, "Lifestyle")
-                this.putInt(EXTRA_STOCK, 7)
-                it.findNavController().navigate(R.id.action_categoryFragment_to_detailCategoryFragment, this)
-            }
+            it.findNavController().navigate(
+                CategoryFragmentDirections.actionCategoryFragmentToDetailCategoryFragment(
+                    "Lifestyle", 7
+                )
+            )
         }
     }
 }
--- app/src/main/java/com/mfitrahrmd/dicoding_belajar_fundamental_aplikasi_android/DetailCategoryFragment.kt
+++ app/src/main/java/com/mfitrahrmd/dicoding_belajar_fundamental_aplikasi_android/DetailCategoryFragment.kt
@@ -1,19 +1,17 @@
 package com.mfitrahrmd.dicoding_belajar_fundamental_aplikasi_android
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.mfitrahrmd.dicoding_belajar_fundamental_aplikasi_android.databinding.FragmentDetailCategoryBinding
 
 class DetailCategoryFragment : Fragment() {
     private lateinit var _binding: FragmentDetailCategoryBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentDetailCategoryBinding.inflate(inflater, container, false)
 
@@ -23,8 +21,8 @@ class DetailCategoryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val name = arguments?.getString(CategoryFragment.EXTRA_NAME)
-        val stock = arguments?.getInt(CategoryFragment.EXTRA_STOCK)
+        val name = DetailCategoryFragmentArgs.fromBundle(arguments as Bundle).name
+        val stock = DetailCategoryFragmentArgs.fromBundle(arguments as Bundle).stock
 
         with(_binding) {
             this.tvCategoryName.text = name
