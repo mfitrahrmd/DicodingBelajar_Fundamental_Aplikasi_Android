1b45ee49d2fddf389d75357f8fcffa5272de1bd6
mfitrahrmd <tgcfitrah26@gmail.com>
2023-12-15 23:31:47 +0700

    navigation popup behaviour

--- app/src/main/java/com/mfitrahrmd/dicoding_belajar_fundamental_aplikasi_android/DetailCategoryFragment.kt
+++ app/src/main/java/com/mfitrahrmd/dicoding_belajar_fundamental_aplikasi_android/DetailCategoryFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.mfitrahrmd.dicoding_belajar_fundamental_aplikasi_android.databinding.FragmentDetailCategoryBinding
 
 class DetailCategoryFragment : Fragment() {
@@ -27,6 +28,9 @@ class DetailCategoryFragment : Fragment() {
         with(_binding) {
             this.tvCategoryName.text = name
             this.tvCategoryDescription.text = "Stock : $stock"
+            this.btnToHome.setOnClickListener {
+                Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment)
+            }
         }
     }
 }
