8fe08706bb2c2ffe89b73f41ba153a3c87735b13
mfitrahrmd <tgcfitrah26@gmail.com>
2023-12-17 13:17:12 +0700

    option menu on appbar

--- app/src/main/java/com/mfitrahrmd/dicoding_belajar_fundamental_aplikasi_android/MainActivity.kt
+++ app/src/main/java/com/mfitrahrmd/dicoding_belajar_fundamental_aplikasi_android/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.mfitrahrmd.dicoding_belajar_fundamental_aplikasi_android
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.mfitrahrmd.dicoding_belajar_fundamental_aplikasi_android.databinding.ActivityMainBinding
 
@@ -9,6 +13,34 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setSupportActionBar(_binding.topAppBar)
         setContentView(_binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu1 -> {
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.fContainer, MenuFragment())
+                    .addToBackStack(null)
+                    .commit()
+
+                true
+            }
+            R.id.menu2 -> {
+                startActivity(Intent(this@MainActivity, MenuActivity::class.java))
+
+                true
+            }
+            else -> false
+        }
     }
 }
---
+++ app/src/main/java/com/mfitrahrmd/dicoding_belajar_fundamental_aplikasi_android/MenuActivity.kt
@@ -0,0 +1,11 @@
+package com.mfitrahrmd.dicoding_belajar_fundamental_aplikasi_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
