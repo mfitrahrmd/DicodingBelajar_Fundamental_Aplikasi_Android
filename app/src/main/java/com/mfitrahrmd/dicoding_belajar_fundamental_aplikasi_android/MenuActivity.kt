6c189868a0ecb23071fb510aad1249c23f9be62d
mfitrahrmd <tgcfitrah26@gmail.com>
2023-12-17 13:38:57 +0700

    searchbar & searchview

--- app/src/main/java/com/mfitrahrmd/dicoding_belajar_fundamental_aplikasi_android/MenuActivity.kt
+++ app/src/main/java/com/mfitrahrmd/dicoding_belajar_fundamental_aplikasi_android/MenuActivity.kt
@@ -2,10 +2,28 @@ package com.mfitrahrmd.dicoding_belajar_fundamental_aplikasi_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.mfitrahrmd.dicoding_belajar_fundamental_aplikasi_android.databinding.ActivityMenuBinding
 
 class MenuActivity : AppCompatActivity() {
+    private lateinit var _binding: ActivityMenuBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        _binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+
+        with(_binding) {
+            searchView.setupWithSearchBar(searchBar)
+            searchView
+                .editText
+                .setOnEditorActionListener { v, actionId, event ->
+                    searchBar.setText(searchView.text)
+                    searchView.hide()
+                    Toast.makeText(this@MenuActivity, searchView.text, Toast.LENGTH_SHORT).show()
+
+                    false
+                }
+        }
     }
 }
